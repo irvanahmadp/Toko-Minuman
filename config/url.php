dc32e0f36398d6c03d06583d79e48d7d274c9aa8
Irvan Ahmad Prasetya <irvan.ap1618@gmail.com>
2019-06-16 21:45:26 +0700

    remove all

--- config/url.php
+++
@@ -1,3 +0,0 @@
-<?php
-	$base_url = 'http://localhost/3.Minuman/';
-?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-  session_destroy();
-  include 'config/url.php';
-  header("Location:".$base_url.'login.php');
-?>
