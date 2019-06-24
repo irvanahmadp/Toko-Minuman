a94453e28273701d73a272b8c0907b81dc95a679
Irvan Ahmad Prasetya <irvan.ap1618@gmail.com>
2019-06-24 09:20:29 +0700

    update

--- function/check-login.php
+++ function/check-login.php
@@ -4,14 +4,14 @@
   $url_arr = explode('/', $url);
   if(!isset($_SESSION['nama']) || $_SESSION['nama'] == ''){
     /* Jika User Belum Login */
-    //if($url_arr[2] != 'login.php' && $url_arr[2] != 'register.php' && $url_arr[2] != 'index.php'){
-    if($url_arr[5] != 'login.php' && $url_arr[5] != 'register.php' && $url_arr[5] != 'index.php'){
+    if($url_arr[2] != 'login.php' && $url_arr[2] != 'register.php' && $url_arr[2] != 'index.php'){
+    //if($url_arr[5] != 'login.php' && $url_arr[5] != 'register.php' && $url_arr[5] != 'index.php'){
       header("Location:".$base_url.'index.php');
     }
   }else{
     /* Jika User Sudah Login */
-    //if($url_arr[2] == 'login.php' || $url_arr[2] == 'register.php' || $url_arr[2] == 'index.php' || $url_arr[2] == ''){
-    if($url_arr[5] == 'login.php' || $url_arr[5] == 'register.php' || $url_arr[5] == 'index.php'){
+    if($url_arr[2] == 'login.php' || $url_arr[2] == 'register.php' || $url_arr[2] == 'index.php' || $url_arr[2] == ''){
+    //if($url_arr[5] == 'login.php' || $url_arr[5] == 'register.php' || $url_arr[5] == 'index.php'){
       header("Location:".$base_url.'produk.php');
     }
   }
